dda86eafc06699d8930b9eb3ba81f13479fa3ab9
alfchaval <59885409+Helio4927@users.noreply.github.com>
2024-06-13 17:22:49 +0200

    WIP Helio 01

---
+++ Assets/- MetaMedicsVR -/Scripts/ControladorCaso1Surepal.cs
@@ -0,0 +1,308 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorCaso1Surepal : MonoBehaviour
+{
+
+    #region ReconocerDispositivo
+    public void StartReconocerDispositivo() 
+    {
+    
+    }
+
+    public void ReconocerDispositivo() 
+    {
+    
+    }
+
+    public void AparecerNevera() 
+    {
+    
+    }
+
+    public void SacarDispositivoDeNevera() 
+    {
+    
+    }
+
+    #endregion
+
+    #region SacarPiezas
+    public void StartSacarPiezas() 
+    {
+    
+    }
+
+    public void SacarAgujas() 
+    {
+    
+    }
+    public void ColocarAgujasEnMesa()
+    {
+
+    }
+    public void SacarOcultadorAgujas() 
+    {
+    
+    }
+    public void ColocarAgujasEnMesaOcultadorAgujasEnMesa()
+    {
+
+    }
+
+    public void SacarCartuchoDeHormona()
+    {
+
+    }
+
+    public void ColocarCartuchoDeHormonaEnMesa()
+    {
+
+    }
+
+    public void SacaSurepal()
+    {
+
+    }
+
+    public void QuitaTapaASurepal()
+    {
+
+    }
+
+    public void ColocarTapaASurepalEnEstuche()
+    {
+
+    }
+
+    public void DesenroscarAnilloDeCierreSurepal()
+    {
+
+    }
+
+    public void ColocarAnilloDeCierreSurepalEnMesa()
+    {
+
+    }
+
+    public void ColocarSurepalEnMesa()
+    {
+
+    }
+
+    public void DisplayNombreDeTodasLasPartes() 
+    {
+    
+    }
+
+    public void MostrarContenedorAgujas()
+    {
+
+    }
+    public void MostrarToallitasAlcoholicas()
+    {
+
+    }
+
+    #endregion
+
+    #region PrepararSurepal
+
+
+    public void StartPrepararSurepal()
+    {
+
+    }
+
+    public void AparecenAnilloDeCierreConCartuchoAlineados()
+    {
+
+    }
+
+    public void DragAndDropCartuchoACierre()
+    {
+
+    }
+
+    public void GirarCartuchoColocadoALaIzquierda()
+    {
+
+    }
+
+    public void TirarHaciaAbajoDeCartuchoColocado()
+    {
+
+    }
+
+    public void AparecenSurepalYAnilloDeCierreAlineados()
+    {
+
+    }
+
+    public void DragAndDropEncajaAnilloDeCierreConSurepal()
+    {
+
+    }
+
+    public void GiraAnilloDeCierreEnSurepal()
+    {
+
+    }
+
+    #endregion
+
+    #region ComoPonermeLaInyeccion
+
+    public void StartComoPonermeLaInyeccion() 
+    {
+    
+    }
+
+    public void MostrarSurepalAlineadoConToallita() 
+    {
+    
+    }
+
+    public void DragAndDropToallitaEnSurepal() 
+    {
+    
+    }
+
+    public void ColocaSurepalSobreLaMesaPostToallita() 
+    {
+    
+    }
+
+    public void MostrarAgujaEmpaquetada() 
+    {
+    
+    }
+
+    public void DespegarPegatinaAgujaEmpaquetada() 
+    {
+    
+    }
+
+    public void MostrarSurepalAlineadoConAgujaAbierta() 
+    {
+    
+    }
+
+    public void DragAndropAgujaAlSurepal() 
+    {
+    
+    }
+
+    public void GiraAgujaEmpaquetadaEnSurepal() 
+    {
+    
+    }
+
+    public void DragAndDropSacaEnvoltorioAgujaDeSurepal() 
+    {
+    
+    }
+
+    public void DejarEnvoltorioDeAgujaEnLaMesa() 
+    {
+    
+    }
+
+    public void MostrarOcultadorDeAgujasAlineadoConSurepal() 
+    {
+    
+    }
+
+    public void DragAndDropOcultadorDeagujaEnSurepal() 
+    {
+    
+    }
+
+    public void GiraBotonDosisSurepal() 
+    {
+    
+    }
+
+    public void ElegirParteDelCuerpoParaPinchar() 
+    {
+    
+    }
+
+
+
+    #endregion
+
+
+    #region PoniendomeLaInyeccion
+
+    public void StartPoninedomeLaInyeccion() 
+    {
+    
+    }
+
+    public void MostrarToallitaConAlcohol() 
+    {
+    
+    }
+
+    public void DragAndDropToallitaAlcoholEnParteDelCuerpoBrazo() 
+    {
+    
+    }
+
+    public void MuestraSurepalConParteDelCuerpoBrazo()
+    {
+
+    }
+
+    public void EchaHaciaAtrasOcultadorDeAguja() 
+    {
+    
+    }
+
+    public void DragAndDropSacarProtectorDeAguja() 
+    {
+    
+    }
+
+    public void MostrarSetupPinchadoBrazo() 
+    {
+    
+    }
+
+    public void SelecionarAnguloPinchado() 
+    {
+    
+    }
+
+    public void DragAndDropPinchaEnElAnguloSeleccionado() 
+    {
+    
+    }
+
+    public void PulsaElBotonSurepalMientrasPincha() 
+    {
+    
+    }
+
+    public void IniciaCuentaAtrasBotonSurepal() 
+    {
+    
+    }
+
+    public void InterrumpeCuentaAtrasBotonSurepal() 
+    {
+    
+    }
+
+    #endregion
+
+    #region PostInyeccion
+    public void StartPostInyeccion() 
+    {
+    
+    }
+    #endregion
+
+}
